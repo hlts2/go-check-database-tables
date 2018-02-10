249886486142f1a69c9729325a8a968af299ff9c
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-02-10 21:56:20 +0900

    fix

---
+++ cmd/ls.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/briandowns/spinner"
+	"github.com/hlts2/go-check-database-tables/dao/databases/config"
+	"github.com/hlts2/go-check-database-tables/dao/factories"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+var lsCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "List for database tables",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := ls(cmd, args); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lsCmd)
+}
+
+func ls(cmd *cobra.Command, args []string) error {
+	c := config.Config{
+		Host:     host,
+		Port:     port,
+		User:     user,
+		Password: password,
+		Database: database,
+	}
+
+	progress := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	progress.Start()
+
+	dao := factories.FactoryTableDao(databaseType, c)
+	tables, err := dao.GetTables()
+	if err != nil {
+		progress.Stop()
+		return err
+	}
+
+	writer := tablewriter.NewWriter(os.Stdout)
+	writer.SetHeader([]string{"Table Name"})
+
+	for _, table := range tables {
+		writer.Append([]string{table.Name})
+	}
+
+	progress.Stop()
+	writer.Render()
+
+	return nil
+}
--- cmd/root.go
+++ cmd/root.go
@@ -3,13 +3,6 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"time"
-
-	"github.com/briandowns/spinner"
-	"github.com/olekukonko/tablewriter"
-
-	"github.com/hlts2/go-check-database-tables/dao/databases/config"
-	"github.com/hlts2/go-check-database-tables/dao/factories"
 
 	"github.com/spf13/cobra"
 )
@@ -17,12 +10,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "go-check-database-tables",
 	Short: "A CLI Tool for checking database tables",
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := root(cmd, args); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	},
 }
 
 var host string
@@ -41,42 +28,10 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&database, "database", "d", "", "Database Name")
 }
 
-func root(cmd *cobra.Command, args []string) error {
-	c := config.Config{
-		Host:     host,
-		Port:     port,
-		User:     user,
-		Password: password,
-		Database: database,
-	}
-
-	progress := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
-	progress.Start()
-
-	dao := factories.FactoryTableDao(databaseType, c)
-	tables, err := dao.GetTables()
-	if err != nil {
-		progress.Stop()
-		return err
-	}
-
-	writer := tablewriter.NewWriter(os.Stdout)
-	writer.SetHeader([]string{"Table Name"})
-
-	for _, table := range tables {
-		writer.Append([]string{table.Name})
-	}
-
-	progress.Stop()
-	writer.Render()
-
-	return nil
-}
-
 //Execute run command
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprint(os.Stderr, err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
